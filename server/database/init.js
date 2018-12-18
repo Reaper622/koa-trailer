7b772bb02be408b9e6c503363262e5c367ff5436
libowen <1064416560@qq.com>
2018-12-18 21:43:00 +0800

    :alembic: 增加MongoDB存储

---
+++ server/database/init.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const db = 'mongodb://localhost/douban-trailer'
+
+mongoose.Promise = global.Promise
+
+exports.connect = () => {
+  let maxConnectTimes = 0
+
+  return new Promise((resolve, reject) => {
+    if(process.env.NODE_ENV !== 'production'){
+      mongoose.set('debug',true)
+    }
+  
+    mongoose.connect(db,{
+      useNewUrlParser: true
+    })
+  
+    mongoose.connection.on('disconnected', () => {
+      maxConnectTimes++
+
+      if (maxConnectTimes < 5){
+        mongoose.connect(db)
+      } else {
+        throw new Error('数据库挂了？修一波')
+      }
+    })
+  
+    mongoose.connection.on('err', err => {
+      if (maxConnectTimes < 5){
+        mongoose.connect(db)
+      } else {
+        reject(err)
+        throw new Error('数据库挂了？修一波')
+      }
+    })
+  
+    mongoose.connection.once('open', () => {
+      // const Dog = mongoose.model('Dog',{ name: String })
+      // const doga = new Dog({ name: '阿尔法'})
+
+      // doga.save().then( () => {
+      //   console.log('汪!');
+      // })
+      resolve()
+      console.log('MongoDB Connected successfully!')
+    })
+  })
+}
